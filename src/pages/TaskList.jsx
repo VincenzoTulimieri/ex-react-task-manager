7feff22f43489ed6ee05c388c8a47afdf6074f70
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-03 14:33:50 +0200

    feat. tasks nella pagina task list

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,4 +1,11 @@
+import { useContext } from "react"
+import { GlobalContext } from "../context/GlobalContext"
+
 export default function TaskList() {
+
+    const {task}= useContext(GlobalContext)
+
+
     return (
         <>
             <h1>Task</h1>
