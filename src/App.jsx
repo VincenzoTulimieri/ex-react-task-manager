09ef9adf91e2c1ca23e3da5063897046a73bb969
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-03 14:33:10 +0200

    feat: provider context

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import TaskList from './pages/TaskList'
 import AddTask from './pages/AddTask'
 import DefaultLayout from './layout/DefaultLayout'
+import { GlobalProvider } from './context/GlobalContext'
 
 
 function App() {
 
   return (
     <>
+    <GlobalProvider>
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout/>}>
@@ -16,6 +18,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+    </GlobalProvider>
     </>
   )
 }
