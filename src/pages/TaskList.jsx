757a38fe32d6083aa1dd46a5eca3ead958b892df
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-06 18:46:26 +0200

    feat: aggiunto debounce

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,12 +1,27 @@
-import { useContext, memo, useState, useMemo } from "react"
+import { useContext, memo, useState, useMemo, useCallback } from "react"
 import { GlobalContext } from "../context/GlobalContext"
 import TaskRow from "../components/TaskRow"
+
+// funzione di debounce
+function debounce(callback, delay) {
+    let timer;
+    return (value) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            callback(value);
+        }, delay);
+    }
+}
 
 export default function TaskList() {
     const { tasks } = useContext(GlobalContext)
 
     const [sortBy, setSortBy] = useState('createdAt')
     const [sortOrder, setSortOrder] = useState(1)
+    const [searchQuery, setSearchQuery] = useState('')
+
+    // debounce di setSearchQuery
+    const debounceSearch = useCallback(debounce(setSearchQuery, 500), [])
 
     // MEMO
     const TaskRowMemo = memo(TaskRow)
@@ -23,7 +38,10 @@ export default function TaskList() {
     }
 
     const sortTask = useMemo(() => {
-        return [...tasks].sort((a, b) => {
+        return [...tasks].filter(task => {
+            const isInTitle = task.title.toLowerCase().includes(searchQuery.toLocaleLowerCase())
+            return isInTitle
+        }).sort((a, b) => {
             let comparison
             if (sortBy === 'title') {
                 comparison = a.title.localeCompare(b.title)
@@ -39,11 +57,16 @@ export default function TaskList() {
             return comparison * sortOrder
         })
 
-    }, [tasks, sortBy, sortOrder])
+    }, [tasks, sortBy, sortOrder, searchQuery])
 
     return (
         <>
-            <h1 className="mb-3">Lista delle Task</h1>
+            <div className="container-h1-input">
+                <h1 className="mb-3">Lista delle Task</h1>
+                <div>
+                    <input type="text" className="form-control mb-2" onChange={(e) => debounceSearch(e.target.value)} placeholder="Cerca" />
+                </div>
+            </div>
             <table className="table table-light table-bordered">
                 <thead>
                     <tr>
