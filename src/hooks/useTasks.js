d18d67c224c7b7db137427f4f361e8ecebebe040
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-06 15:46:40 +0200

    feat: funzione updateTask creata

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -12,31 +12,43 @@ export default function useTasks() {
     }, [])
 
     const addTask = async (newTask) => {
-        const response = await fetch(`${VITE_URL_API}/tasks`,{
+        const response = await fetch(`${VITE_URL_API}/tasks`, {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(newTask)
         })
-        const{success,message,task} = await response.json()
-        if(!success){
+        const { success, message, task } = await response.json()
+        if (!success) {
             throw new Error(message)
         }
-        setTasks(prev=>[...prev, task])
+        setTasks(prev => [...prev, task])
     }
     const removeTask = async (taskId) => {
-        const response = await fetch(`${VITE_URL_API}/tasks/${taskId}`,{
+        const response = await fetch(`${VITE_URL_API}/tasks/${taskId}`, {
             method: 'DELETE',
         })
-        const{success,message} = await response.json()
-        if(!success){
+        const { success, message } = await response.json()
+        if (!success) {
             throw new Error(message)
         }
-        
+
         // rimozione di un elemento specifico in un array
-        setTasks(prev=> prev.filter(t=>t.id !== taskId))
+        setTasks(prev => prev.filter(t => t.id !== taskId))
     }
-    const updateTask = () => { }
+
+    const updateTask = async (updateTask) => {
+        const response = await fetch(`${VITE_URL_API}/tasks/${updateTask.id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(updateTask)
+        })
+        const { success, message, newTask } = await response.json()
+        if (!success) {
+            throw new Error(message)
+        }
+        setTasks(prev=>prev.map(oldTask=> oldTask.id === newTask.id ? newTask : oldTask))
+    }
 
 
-    return {tasks, addTask, removeTask, updateTask}
+    return { tasks, addTask, removeTask, updateTask }
 }
