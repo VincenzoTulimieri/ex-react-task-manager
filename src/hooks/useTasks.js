9b8e7ddffaa1243d3820a6e0f9b98cf0855995c4
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-09-06 16:35:27 +0200

    fix: errore in updateTask

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -42,7 +42,7 @@ export default function useTasks() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(updateTask)
         })
-        const { success, message, newTask } = await response.json()
+        const { success, message, task: newTask } = await response.json()
         if (!success) {
             throw new Error(message)
         }
